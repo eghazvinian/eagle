e3a94466c20e3c2360e723e04d277b9a90baf1fb
Ehsan <ehsan@mdeng.ca>
2016-04-05 17:24:26 -0400

    Beginning the EAGLE

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-Stork
+Eagle
 =====
 
-"Fork Stork" to create a new MOOSE-based application.
+"Fork Eagle" to create a new MOOSE-based application.
 
 For more information see: [http://mooseframework.org/create-an-app/](http://mooseframework.org/create-an-app/)
--- src/base/EagleApp.C
+++ src/base/EagleApp.C
@@ -1,11 +1,11 @@
-#include "StorkApp.h"
+#include "EagleApp.h"
 #include "Moose.h"
 #include "AppFactory.h"
 #include "ModulesApp.h"
 #include "MooseSyntax.h"
 
 template<>
-InputParameters validParams<StorkApp>()
+InputParameters validParams<EagleApp>()
 {
   InputParameters params = validParams<MooseApp>();
 
@@ -16,40 +16,40 @@ InputParameters validParams<StorkApp>()
   return params;
 }
 
-StorkApp::StorkApp(InputParameters parameters) :
+EagleApp::EagleApp(InputParameters parameters) :
     MooseApp(parameters)
 {
   Moose::registerObjects(_factory);
   ModulesApp::registerObjects(_factory);
-  StorkApp::registerObjects(_factory);
+  EagleApp::registerObjects(_factory);
 
   Moose::associateSyntax(_syntax, _action_factory);
   ModulesApp::associateSyntax(_syntax, _action_factory);
-  StorkApp::associateSyntax(_syntax, _action_factory);
+  EagleApp::associateSyntax(_syntax, _action_factory);
 }
 
-StorkApp::~StorkApp()
+EagleApp::~EagleApp()
 {
 }
 
 // External entry point for dynamic application loading
-extern "C" void StorkApp__registerApps() { StorkApp::registerApps(); }
+extern "C" void EagleApp__registerApps() { EagleApp::registerApps(); }
 void
-StorkApp::registerApps()
+EagleApp::registerApps()
 {
-  registerApp(StorkApp);
+  registerApp(EagleApp);
 }
 
 // External entry point for dynamic object registration
-extern "C" void StorkApp__registerObjects(Factory & factory) { StorkApp::registerObjects(factory); }
+extern "C" void EagleApp__registerObjects(Factory & factory) { EagleApp::registerObjects(factory); }
 void
-StorkApp::registerObjects(Factory & factory)
+EagleApp::registerObjects(Factory & factory)
 {
 }
 
 // External entry point for dynamic syntax association
-extern "C" void StorkApp__associateSyntax(Syntax & syntax, ActionFactory & action_factory) { StorkApp::associateSyntax(syntax, action_factory); }
+extern "C" void EagleApp__associateSyntax(Syntax & syntax, ActionFactory & action_factory) { EagleApp::associateSyntax(syntax, action_factory); }
 void
-StorkApp::associateSyntax(Syntax & /*syntax*/, ActionFactory & /*action_factory*/)
+EagleApp::associateSyntax(Syntax & /*syntax*/, ActionFactory & /*action_factory*/)
 {
 }
